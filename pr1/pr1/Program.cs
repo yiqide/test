4663f3f119da646c316a8d2e29e8721ecff30ae7
ddw <2780624596@qq.com>
2021-07-09 13:30:05 +0800

    test

---
+++ pr1/pr1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace pr1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("哈哈哈哈");
+        }
+    }
+}
